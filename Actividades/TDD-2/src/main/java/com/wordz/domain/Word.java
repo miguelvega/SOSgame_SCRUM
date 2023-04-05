922d8f88e80ccacda06bbad7bc5e344306eee97f
miguelvega <124398378+miguelvega@users.noreply.github.com>
2023-04-05 09:41:05 -0500

    Subiendo Actividad 2

---
+++ Actividades/TDD-2/src/main/java/com/wordz/domain/Word.java
@@ -0,0 +1,14 @@
+package com.wordz.domain;
+
+public class Word {
+    public Word(String correctWord) {
+        // No Implementado
+    }
+
+
+    public Score guess(String attempt) {
+        var score = new Score();
+        return score;
+    }
+}
+
