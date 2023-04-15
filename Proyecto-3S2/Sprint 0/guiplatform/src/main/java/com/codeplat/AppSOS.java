32fbbf210750416f050284cd7840fd5d7a4404b6
miguelvega <124398378+miguelvega@users.noreply.github.com>
2023-04-15 00:30:13 -0500

    Subiendo Sprint 0
    Trabajo en conjunto con Arturo Hinostroza

---
+++ Proyecto-3S2/Sprint 0/guiplatform/src/main/java/com/codeplat/AppSOS.java
@@ -0,0 +1,23 @@
+package com.codeplat;
+
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AppSOS{
+    private int n=5;
+    private JPanel PanelPrincipal;
+    private JRadioButton SimpleGameButton;
+    private JRadioButton GeneralGameButton;
+    private JTextField NXN;
+    private JRadioButton Sblue;
+    private JRadioButton Oblue;
+    private JRadioButton SRed;
+    private JRadioButton ORed;
+    private JButton NewGameButton;
+    private JTextPane PlayersTurn;
+    private JPanel TableroNXN;
+    private JTable Tablero;
+
+}
---
+++ Proyecto-3S2/Sprint 0/guiplatform/src/main/java/com/codeplat/TableroNXN.java
@@ -0,0 +1,4 @@
+package com.codeplat;
+
+public class TableroNXN {
+}
