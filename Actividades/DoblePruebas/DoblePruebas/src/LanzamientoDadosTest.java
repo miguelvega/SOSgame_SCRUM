8c81cca623c693f0330848c93f3c3c117cb4cf87
miguelvega <124398378+miguelvega@users.noreply.github.com>
2023-06-05 12:07:22 -0500

    Agregar comentario LanzamientoDadosTest.java

--- Actividades/DoblePruebas/DoblePruebas/src/LanzamientoDadosTest.java
+++ Actividades/DoblePruebas/DoblePruebas/src/LanzamientoDadosTest.java
@@ -5,6 +5,7 @@ class LanzamientoDadosTest {
     @Test
     void producesMensage() {
         var stub = new StubNumeroAleatorio();// inyectamos un doble de prueba: una instancia de la clase
+                                             // que hemos escrito específicamente para que sea fácil de usar en una prueba unitaria
         var lanzado = new LanzamientoDados(stub);
         var actual = lanzado.asText();
         assertThat(actual).isEqualTo("Tu lanzamiento a 5");
